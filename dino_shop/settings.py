cb2f15b1ed9348745707464e6378d82daed4ade3
EmcioN <villly1990@gmail.com>
2023-11-13 17:28:59 +0000

    added home app and templates, add nav bar

--- dino_shop/settings.py
+++ dino_shop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'home',
 
 ]
 
@@ -63,7 +64,10 @@ ROOT_URLCONF = 'dino_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
