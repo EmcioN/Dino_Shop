757dcf905d19023b2e19160b579ca2793531c8e9
EmcioN <villly1990@gmail.com>
2023-12-01 22:13:40 +0000

    Add delete dino button with logic to it, add confirm delete html

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('dinosaurs/', views.all_dinosaurs, name='all_dinosaurs'),
     path('add/', views.add_dinosaur, name='add_dinosaur'),
     path('dinosaur/<int:dinosaur_id>/', views.item_page, name='item_page'),
+    path('dinosaur/delete/<int:dinosaur_id>/', views.delete_dinosaur, name='delete_dinosaur'),
       
 ]
