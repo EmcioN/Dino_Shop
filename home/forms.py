2c0b35211ab92c06c3f4e5107a8d8975186f41cd
EmcioN <villly1990@gmail.com>
2024-01-09 00:12:32 +0000

    Add form for newsletter

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from .models import NewsletterSubscriber
 
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
+
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = NewsletterSubscriber
+        fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'})
+        }
