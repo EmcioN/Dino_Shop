2edc41e08fdaec8e49d65ed2d9de7bccc77c373d
EmcioN <villly1990@gmail.com>
2024-01-09 00:39:06 +0000

    Update urls adding path to subscribe page

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('success/', views.success, name='success_url'),
     path('patch-notes/', views.patch_notes, name='patch_notes'),
+    path('subscribe/', views.subscribe_newsletter, name='subscribe_newsletter'),
 ]
