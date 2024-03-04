0d6a77ac799c0d439f3a61c99377378b9f2ee3b1
EmcioN <villly1990@gmail.com>
2024-03-04 17:53:32 +0000

    change list of details in admin panel

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,12 +1,12 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import OrderLineItem
+from .models import OrderItem
 
 
-@receiver(post_save, sender=OrderLineItem)
+@receiver(post_save, sender=OrderItem)
 def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
-@receiver(post_delete, sender=OrderLineItem)
+@receiver(post_delete, sender=OrderItem)
 def update_on_delete(sender, instance, **kwargs):
     instance.order.update_total()
